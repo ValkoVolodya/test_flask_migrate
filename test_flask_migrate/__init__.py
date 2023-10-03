866d75131a4afb4616bacc6f40290455a4b40567
v.valko <v.valko@smartweb.com.ua>
2023-10-03 16:06:51 +0300

    [fix] flask factory function now used properly

--- test_flask_migrate/__init__.py
+++ test_flask_migrate/__init__.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
 db = SQLAlchemy()
 migrate = Migrate()
-
 
 
 def create_app():
@@ -17,3 +15,6 @@ def create_app():
     migrate.init_app(app, db)
 
     return app
+
+
+app = create_app()
